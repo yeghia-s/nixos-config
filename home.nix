c0edb8a1a540c07600c84686da3a20db25f72c74
Yeghia <yeghiasargis@yahoo.com>
2026-03-06 20:52:09 -0500

    Add Hyprland config, dark mode, Home Manager

--- home.nix
+++ home.nix
@@ -29,6 +29,28 @@ home.pointerCursor = {
   size = 24;
 };
 
+gtk = {
+  enable = true;
+  theme = {
+    name = "adw-gtk3-dark";
+    package = pkgs.adw-gtk3;
+  };
+  gtk3.extraConfig.gtk-application-prefer-dark-theme = true;
+  gtk4.extraConfig.gtk-application-prefer-dark-theme = true;
+};
+
+dconf.settings = {
+  "org/gnome/desktop/interface" = {
+    color-scheme = "prefer-dark";
+  };
+};
+
+  qt = {
+    enable = true;
+    platformTheme.name = "gtk";
+    style.name = "adwaita-dark";
+  };
+
 wayland.windowManager.hyprland = {
   enable = true;
   settings = {
