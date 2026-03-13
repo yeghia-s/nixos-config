2f524b4927ccae64fe586835ef8f2fd7fb2689b5
Yeghia <yeghiasargis@yahoo.com>
2026-03-13 03:17:54 -0400

    add unzip

--- home/packages.nix
+++ home/packages.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }:
 {
   home.packages = with pkgs; [
+    unzip             # zip file tool
     jre               # java
     jq                # json
     wlr-randr         # resolution info
